f641c6dd8330e8de6bf8da8f6db417e308ca17fb
ujjaval01 <sainiujvl@gmail.com>
2025-02-14 20:40:05 +0530

    update activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.ui.rakshakawatch"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ui.rakshakawatch"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/ui/rakshakawatch/SplashActivity.kt
+++ app/src/main/java/com/ui/rakshakawatch/SplashActivity.kt
@@ -1,6 +1,10 @@
 package com.ui.rakshakawatch
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,22 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
+        // Ensure findViewById is called after setContentView
+        val textView = findViewById<TextView>(R.id.runningTxt)
+        textView.isSelected = true
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Use Handler with Looper.getMainLooper() to avoid deprecation
+        Handler(Looper.getMainLooper()).postDelayed({
+            val iSplash = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(iSplash)
+            finish()
+        }, 4000)
     }
 }
