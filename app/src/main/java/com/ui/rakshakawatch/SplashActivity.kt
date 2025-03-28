f6a150d4cc9507494ab0741060a4cf69077bb7e7
ujjaval01 <sainiujvl@gmail.com>
2025-03-28 23:53:00 +0530

    update splash code

--- app/src/main/java/com/ui/rakshakawatch/SplashActivity.kt
+++ app/src/main/java/com/ui/rakshakawatch/SplashActivity.kt
@@ -5,41 +5,30 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
 
         // declared id's
         val textView = findViewById<TextView>(R.id.runningTxt)
         textView.isSelected = true
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
         // Use Handler with Looper.getMainLooper() to avoid deprecation
         Handler(Looper.getMainLooper()).postDelayed({
-            val sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE)
-            val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+            val auth = FirebaseAuth.getInstance()
 
-            if (isLoggedIn) {
-                // If logged in, open MainActivity
+            if (auth.currentUser != null) {
+                // User is already logged in, open MainActivity
                 startActivity(Intent(this, MainActivity::class.java))
             } else {
-                // If not logged in, open LoginActivity
+                // User is not logged in, open LoginActivity
                 startActivity(Intent(this, LoginActivity::class.java))
             }
             finish() // Close SplashActivity
-        }, 2500) // 2 seconds delay
-
+        }, 2500) // 2.5 seconds delay
     }
 }
